797a68385905847c72ea959311457b41da6c6689
Paweł Jasiaczyk <pawel.jasiaczyk@gmail.com>
2017-07-01 23:19:29 +0200

    Basic verification of transaction works well - transaction on P24 side is accepted

--- P24MvcClient/Models/OnePageClientViewModel.cs
+++ P24MvcClient/Models/OnePageClientViewModel.cs
@@ -51,6 +51,12 @@ namespace P24MvcClient.Models
         {
             this.P24 = new Przelewy24.Przelewy24();
             this.P24.P24_url_status = HttpContext.Current.Request.Url.Authority + "/home/P24Status";
+#if DEBUG
+            this.P24.MerchantId = 1604;
+            this.P24.PosId = 1604;
+            this.P24.CrcKey = "4aca0b5e3950f78b";
+            this.P24.SandboxMode = true;
+#endif
             this.Transaction = new Transaction(this.P24);
             this.P24.TransactionDb = SingletonDbModel.GetInstace() as IP24Db;
         }
